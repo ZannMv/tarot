ede4be2d88c9fdf5f5f3f10df548955517a39ec0
ZannMv <116694002+ZannMv@users.noreply.github.com>
2025-03-11 10:59:25 +0700

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
       htmlAttrs: {
         lang: "th",
       },
-      title: "Hamtarot",
+      title: "Tarot",
       meta: [
         {
           name: "viewport",
